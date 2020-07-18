1880ad0f9917023bf89b081a2b166cd137e5b3b7
Danny <asddchao@163.com>
2020-07-19 01:33:41 +0800

    'change-path'

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: `./data`
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://asddchao.gitee.io/Xiaomi/data' : '/data'
 })
 
 function getDropList () {
